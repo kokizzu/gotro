0797b45b8fc8754826755394c4f0d1c0d8e3e6ac
Kiswono Prayogo <kiswono@gmail.com>
2017-06-24 12:15:16 +0700

    added L.CreateFile

--- L/File.go
+++ L/File.go
@@ -6,3 +6,25 @@ func FileExists(name string) bool {
 	_, err := os.Stat(name)
 	return !os.IsNotExist(err)
 }
+
+func CreateFile(path string, content string) bool {
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if err != nil {
+		Print(`CreateFile.OpenFile: ` + path + ` | ` + err.Error())
+		return false
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(content)
+	if err != nil {
+		Print(`CreateFile.WriteFile: ` + path + ` | ` + err.Error())
+		return false
+	}
+
+	err = file.Sync()
+	if err != nil {
+		Print(`CreateFile.SyncFile: ` + path + ` | ` + err.Error())
+		return false
+	}
+	return true
+}
