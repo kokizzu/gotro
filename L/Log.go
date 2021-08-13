6d9e36b318b72d1e87c7557cdcb6c8b081aeb1cb
Kiswono Prayogo <kiswono@gmail.com>
2021-08-13 22:42:58 +0700

    add L.CallerInfo

--- L/Log.go
+++ L/Log.go
@@ -5,16 +5,18 @@ package L
 import (
 	"bufio"
 	"fmt"
-	"github.com/fatih/color"
-	"github.com/kokizzu/gotro/I"
-	"github.com/kr/pretty"
-	"github.com/op/go-logging"
 	"os"
 	"os/exec"
+	"path"
 	"runtime"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/fatih/color"
+	"github.com/kokizzu/gotro/I"
+	"github.com/kr/pretty"
+	"github.com/op/go-logging"
 )
 
 var LOG *logging.Logger
@@ -312,4 +314,38 @@ func PipeRunCmd(cmd string, args ...string) error {
 	exe.Stdout = os.Stdout
 	exe.Stderr = os.Stderr
 	return exe.Run()
+}
+
+type CallInfo struct {
+	PackageName string
+	FileName    string
+	FuncName    string
+	Line        int
+}
+
+func CallerInfo(skip ...int) *CallInfo {
+	skipCount := 1
+	if len(skip) > 0 {
+		skipCount = skip[0]
+	}
+	pc, file, line, _ := runtime.Caller(skipCount)
+	_, fileName := path.Split(file)
+	parts := strings.Split(runtime.FuncForPC(pc).Name(), ".")
+	pl := len(parts)
+	packageName := ""
+	funcName := parts[pl-1]
+
+	if parts[pl-2][0] == '(' {
+		funcName = parts[pl-2] + "." + funcName
+		packageName = strings.Join(parts[0:pl-2], ".")
+	} else {
+		packageName = strings.Join(parts[0:pl-1], ".")
+	}
+
+	return &CallInfo{
+		PackageName: packageName,
+		FileName:    fileName,
+		FuncName:    funcName,
+		Line:        line,
+	}
 }
