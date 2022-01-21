60e3aa1aad9d7e43233bdbc9783810e027640767
Andri <m.a.juliansyah@gmail.com>
2022-01-21 20:27:24 +0700

    clickhouse backup revision without restore

--- W2/example/backup_ch.sh
+++ W2/example/backup_ch.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-now=`date '+%Y%m%d_%H%M%S'`;
-echo "andri" | sudo -S clickhouse-backup clean
-echo "andri" | sudo -S clickhouse-backup create
-echo "andri" | sudo tar -czf /var/lib/clickhouse/backup/ch_backup_$now.tgz -C /var/lib/clickhouse/backup/ .
-rsync --delete -r -e "ssh -p 2211" andri@127.0.0.1:/var/lib/clickhouse/backup/* ~/Documents
+set -x
+SUDOPASS="andri"
 
+now=`date '+%Y%m%d_%H%M%S'`;
+isExist="$(which clickhouse-backup)"
+
+if [[ -z "${isExist}" ]] ; then
+	echo $SUDOPASS | sudo -S wget -c https://github.com/AlexAkulov/clickhouse-backup/releases/download/v0.5.2/clickhouse-backup.tar.gz
+	echo $SUDOPASS | sudo -S tar -xf clickhouse-backup.tar.gz
+	cd clickhouse-backup/
+	sudo cp clickhouse-backup /usr/local/bin
+	clickhouse-backup -v
+fi
+
+echo $SUDOPASS | sudo -S rm -rf /var/lib/clickhouse/backup/*
+echo $SUDOPASS | sudo -S clickhouse-backup clean
+echo $SUDOPASS | sudo -S clickhouse-backup create
+echo $SUDOPASS | sudo tar -czf /var/lib/clickhouse/backup/ch_backup_$now.tgz -C /var/lib/clickhouse/backup/ .
--- W2/example/run_backup_ch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh -p 2211 andri@127.0.0.1 'bash -s' < backup_ch.sh
