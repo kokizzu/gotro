383c75f6225c726ea50671ba5818fa2d1566e6fd
Kiswono Prayogo <kiswono@gmail.com>
2022-09-27 03:47:51 +0700

    add meilisearch dockertest

--- D/Mi/meilisearch_dockertest.go
+++
@@ -1,1 +0,0 @@
-package Mi
---
+++ D/Ms/meilisearch_dockertest.go
@@ -0,0 +1,52 @@
+package Ms
+
+import (
+	"net"
+	"time"
+
+	"github.com/kokizzu/gotro/D"
+	"github.com/ory/dockertest/v3"
+)
+
+type MsDockerTest struct {
+	Image string
+	pool  *D.DockerTest
+	Port  string
+}
+
+// https://hub.docker.com/getmeili/meilisearch
+func (in *MsDockerTest) ImageVersion(pool *D.DockerTest, version string) *dockertest.RunOptions {
+	in.pool = pool
+	in.SetDefaults(version)
+	return &dockertest.RunOptions{
+		Repository: `getmeili/meilisearch`,
+		Name:       `dockertest-meili-` + pool.Uniq,
+		Tag:        in.Image,
+		NetworkID:  pool.Network.ID,
+		Env:        []string{},
+	}
+}
+
+func (in *MsDockerTest) ImageLatest(pool *D.DockerTest) *dockertest.RunOptions {
+	return in.ImageVersion(pool, `latest`)
+}
+
+func (in *MsDockerTest) SetDefaults(img string) {
+	if in.Image == `` {
+		in.Image = img
+	}
+}
+
+func (in *MsDockerTest) ConnectCheck(res *dockertest.Resource) (err error) {
+	in.Port = res.GetPort("7700/tcp")
+	hostPort := in.pool.HostPort(in.Port)
+	// using net Dial instead of proper driver
+	var conn net.Conn
+	conn, err = net.DialTimeout("tcp", hostPort, 1*time.Second)
+	if conn != nil {
+		defer func() {
+			_ = conn.Close()
+		}()
+	}
+	return
+}
